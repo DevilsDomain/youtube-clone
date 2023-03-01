57c92de37a012abbe69ac8ce0bf054e39759bec3
DevilsDomain <alaraerzincan@gmail.com>
2023-03-01 15:46:40 +0100

    search works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-
 import './App.css';
+import axios from 'axios'
+import useSWR from 'swr'
+import { useState, useRef } from 'react';
 
+const fetcher = url => axios.get(url).then(res => res.data)
 function App() {
+  const [searchText, setSearchText] = useState('cat')
+  const inputRef = useRef('');
+  const { data, error } = useSWR( `https://youtube.thorsteinsson.is/api/search?q=${searchText}`, fetcher)
+
+  // search bar component
+  function SearchBar() {
+    // handle submit
+    const handleSubmit = () => {
+      let value = inputRef.current.value;
+      setSearchText(value);
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Search video..."
+          ref={inputRef}
+        />
+        <button type="button">
+          search
+        </button>
+      </form>
+    );
+  }
+  
+  console.log(data)
+  if (!data) {
+    return "Loading...";
+  }
+
+  if(error) {
+    return(
+      <p>Oops, looks like we are down!</p>
+    );
+  }
+
   return (
     <div className="App">
-      <p>hello</p>
+
+      <SearchBar></SearchBar>
+        <ul>
+        {data.status !== false ? (
+        <ul>
+          {data.map((item) => {
+            return <li key={item.id}>{item.title}</li>;
+          })}
+        </ul>
+      ) : (
+        <div>No video found with this tile</div>
+      )}
+        </ul>
+
     </div>
   );
 }
